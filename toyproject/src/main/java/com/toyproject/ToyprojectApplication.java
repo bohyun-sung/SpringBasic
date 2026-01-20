729b92cd443dfae4a808bd108dd50dd1f2bdf260
성보현 <djajsl8799@naver.com>
2026-01-20 18:48:22 +0900

    서버 실행 밑 관리자 엔티티 작성..1

--- toyproject/src/main/java/com/toyproject/ToyprojectApplication.java
+++ toyproject/src/main/java/com/toyproject/ToyprojectApplication.java
@@ -1,4 +1,4 @@
-package com.toyproject.toyproject;
+package com.toyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
