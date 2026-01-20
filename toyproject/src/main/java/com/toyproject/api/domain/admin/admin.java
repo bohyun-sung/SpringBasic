802a16cc5cd045103760ca17e705f2d1560437a9
성보현 <djajsl8799@naver.com>
2026-01-20 18:48:28 +0900

    서버 실행 밑 관리자 엔티티 작성..1

---
+++ toyproject/src/main/java/com/toyproject/api/domain/admin/admin.java
@@ -0,0 +1,33 @@
+package com.toyproject.api.domain.admin;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "admin")
+@Entity
+public class admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_idx", columnDefinition = "BIGINT UNSIGNED")
+    private Long idx;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '이름'")
+    private String name;
+
+    @Column(name = "user_id", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '아이디'")
+    private String userId;
+
+    @Column(name = "password", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '비밀번호'")
+    private String password;
+
+    @Column(name = "email", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '이메일'")
+    private String email;
+
+    @Column(name = "phone", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '핸드폰'")
+    private String phone;
+}
