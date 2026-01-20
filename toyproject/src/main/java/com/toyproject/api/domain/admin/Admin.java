a1f7add3eb58079bc4aaf35e61c99234c0ef3ade
성보현 <djajsl8799@naver.com>
2026-01-20 19:04:00 +0900

    생성시간 변경시간 추가

--- toyproject/src/main/java/com/toyproject/api/domain/admin/Admin.java
+++ toyproject/src/main/java/com/toyproject/api/domain/admin/Admin.java
@@ -1,5 +1,6 @@
 package com.toyproject.api.domain.admin;
 
+import com.toyproject.api.domain.base.DefaultTimeStampEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,14 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "admin")
 @Entity
-public class admin {
+public class Admin extends DefaultTimeStampEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "admin_idx", columnDefinition = "BIGINT UNSIGNED")
     private Long idx;
 
-    @Column(name = "name", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '이름'")
+    @Column(name = "name", columnDefinition = "VARCHAR(20) NOT NULL COMMENT '이름'")
     private String name;
 
     @Column(name = "user_id", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '아이디'")
@@ -28,6 +29,6 @@ public class admin {
     @Column(name = "email", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '이메일'")
     private String email;
 
-    @Column(name = "phone", columnDefinition = "VARCHAR(100) NOT NULL COMMENT '핸드폰'")
+    @Column(name = "phone", columnDefinition = "VARCHAR(20) NOT NULL COMMENT '핸드폰'")
     private String phone;
 }
